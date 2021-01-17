c7b9b4f7e25ed1947fd687348b697fceaa5ad826
Saurabh <sp1141997@gmail.com>
2021-01-17 16:10:33 +0530

    added some data and change in index

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// date_default_timezone_set('Asia/Kolkata');
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'balaji';
+
+$con = mysqli_connect($servername,$username,$password,$database) or die(mysqli_error());
+//session_start();
+?>
